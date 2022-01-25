453337d35b8a779548a33b52076e363566143c4d
EstebanJPG <esteban.jimenez.piquerass@gmail.com>
2022-01-25 15:05:09 +0100

    Initial commit

---
+++ src/main/java/psp/esteban/sockets/practica1/App.java
@@ -0,0 +1,13 @@
+package psp.esteban.sockets.practica1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
